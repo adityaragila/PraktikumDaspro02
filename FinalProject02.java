a02296cbd2389a44819e1f94523231000213db49
adityaragila <adityaragil2006@gmail.com>
2025-09-25 10:14:29 +0700

    nyuupdate

--- FinalProject02.java
+++ FinalProject02.java
@@ -1,11 +1,8 @@
 public class FinalProject02 {
     public static void main(String[]args) {
         System.out.println (" Hi, ini baris kode pertama ");
-<<<<<<< HEAD:Test.java
         system.out.println("user 1 menambahkan baris baru");
-        
-=======
+    
         System.out.println("hii ini baris kode ke dua");
->>>>>>> d7b3c57bc291c75d8aecfbfc0361fc926fdd7025:FinalProject02.java
     }
 }
