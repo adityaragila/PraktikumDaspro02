d7b3c57bc291c75d8aecfbfc0361fc926fdd7025
ItsMyIP <bayudika004@gmail.com>
2025-09-25 10:06:59 +0700

    update 2

--- FinalProject02.java
+++ FinalProject02.java
@@ -1,6 +1,6 @@
-public class Test {
+public class FinalProject02 {
     public static void main(String[]args) {
         System.out.println (" Hi, ini baris kode pertama ");
-        system.out.println("hii ini baris kode ke dua");
+        System.out.println("hii ini baris kode ke dua");
     }
 }
