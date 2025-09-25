db205dfa2c144e49013091ad4038e701685e9171
ItsMyIP <bayudika004@gmail.com>
2025-09-25 10:04:16 +0700

    nyuse

--- FinalProject02.java
+++ FinalProject02.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[]args) {
         System.out.println (" Hi, ini baris kode pertama ");
-
+        system.out.println("hii ini baris kode ke dua");
     }
 }
