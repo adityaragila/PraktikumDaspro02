f386dcd6668143b6c2b9eff59297771ebee7d171
adityaragila <adityaragil2006@gmail.com>
2025-10-02 07:36:24 +0700

    modifikasi percobaan 1

--- jobsheet5/ifCetakKRS02.java
+++ jobsheet5/ifCetakKRS02.java
@@ -5,11 +5,18 @@ import java.util.Scanner;
 public class ifCetakKRS02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("--- Cetak KRS SIAKAD ---");
-        System.out.println("Apakah UKT Sudah Lunas? (true/false): ");
-        boolean uktLunas = sc.nextBoolean();
-        System.out.println("Pembayaran UKT terverifikasi");
-        System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
 
+        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.print("Apakah UKT Sudah Lunas? (true/false): ");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+        } else {
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu.");
+        }
+
+        sc.close();
     }
 }
